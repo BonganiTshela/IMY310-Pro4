208199bb0863136fe28463c942f560ca38d35f32
Harris Leshaba <u15312144@tuks.co.za>
2017-05-12 10:54:57 +0200

    gddf
    fdsfgsfd

--- profile/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = "localhost";
-$dbuser = "test";
-$dbpass = "";
-$dbname = "test";
-mysql_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
-mysql_select_db($dbname) or die('database selection problem');
-?>
