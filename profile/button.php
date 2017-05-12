ccc97b297034cdd308436108385e9a1fe4468603
Harris Leshaba <u15312144@tuks.co.za>
2017-05-12 11:21:56 +0200

    Organise

---
+++ profile/button.php
@@ -0,0 +1,418 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>ComeUp a Entertainment Category Flat Bootstrap Responsive Website Template | Blogs :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.css" type='text/css' />
+<!-- //lined-icons -->
+ <!-- Meters graphs -->
+<script src="js/jquery-2.1.4.js"></script>
+
+</head> 
+   <!-- /w3layouts -->
+ <body class="sticky-header left-side-collapsed"  onload="initMap()">
+    <section>
+      <!-- left side start-->
+		<div class="left-side sticky-left-side">
+
+			<!--logo and iconic logo start-->
+			<div class="logo">
+				<h1><a href="index.html">ComeUp<span>c</span></a></h1>
+			</div>
+			<div class="logo-icon text-center">
+				<a href="index.html">ComeUp</a>
+			</div>
+
+			<!--logo and iconic logo end-->
+					<div class="left-side-inner">
+
+				<!--sidebar nav start-->
+					<ul class="nav nav-pills nav-stacked custom-nav">
+						<li class="active"><a href="index.html"><i class="lnr lnr-home"></i><span>Home</span></a></li>
+						
+						<li><a href="radio.html"><i class="camera"></i> <span>Radio</span></a></li>
+						<!--li><a href="#" data-toggle="modal" data-target="#myModal1"><i class="fa fa-th"></i><span>Apps</span></a></li-->
+						<li><a href="radio.html"><i class="lnr lnr-users"></i> <span>Artists</span></a></li> 
+						<li><a href="browse.html"><i class="lnr lnr-music-note"></i> <span>Albums</span></a></li>						
+						<!--li class="menu-list"><a href="browse.html"><i class="lnr lnr-indent-increase"></i> <span>Browser</span></a-->  
+							<ul class="sub-menu-list">
+								<li><a href="browse.html">Artists</a> </li>
+								<li><a href="404.html">Services</a> </li>
+							</ul>
+						</li>
+						<li><a href="blog.html"><i class="lnr lnr-book"></i><span>Blog</span></a></li>
+						<li><a href="typography.html"><i class="lnr lnr-user"></i> <span>Profile</span></a></li>
+						<li class="menu-list"><a href="#"><i class="lnr lnr-heart"></i>  <span>My Favourities</span></a> 
+							<ul class="sub-menu-list">
+								<li><a href="radio.html">All Songs</a></li>
+							</ul>
+						</li>
+						<!--li class="menu-list"><a href="contact.html"><i class="fa fa-thumb-tack"></i><span>Contact</span></a>
+							<ul class="sub-menu-list">
+								<li><a href="contact.html">Location</a> </li>
+							</ul>
+						</li-->     
+					</ul>
+				<!--sidebar nav end-->
+			</div>
+		</div>
+		<!-- left side end-->
+		 <!-- /agileinfo -->
+					<!-- app-->
+			<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog facebook" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body">
+							<div class="app-grids">
+								<div class="app">
+						<div class="col-md-5 app-left mpl">
+							<h3>ComeUp mobile app on your smartphone!</h3>
+							<p>Download and Avail Special Songs Videos and Audios.</p>
+							<div class="app-devices">
+								<h5>Gets the app from</h5>
+								<a href="#"><img src="images/1.png" alt=""></a>
+								<a href="#"><img src="images/2.png" alt=""></a>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-7 app-image">
+							<img src="images/apps.png" alt="">
+						</div>
+						<div class="clearfix"></div>
+					</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //app-->
+			 <!-- /agile-its -->
+		<!-- signup -->
+			<div class="modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+							<div class="sign-grids">
+								<div class="sign">
+									<div class="sign-left">
+										<ul>
+											<li><a class="fb" href="#"><i></i>Sign in with Facebook</a></li>
+											<li><a class="goog" href="#"><i></i>Sign in with Google</a></li>
+											<li><a class="linkin" href="#"><i></i>Sign in with Linkedin</a></li>
+										</ul>
+									</div>
+									<div class="sign-right">
+										<form action="#" method="post">
+											<h3>Create your account </h3>
+											<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+											<input type="text" value="Mobile number" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Mobile number';}" required="">
+											<input type="text" value="Email id" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email id';}" required="">	
+											<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" required="">	
+											
+											<input type="submit" value="CREATE ACCOUNT" >
+										</form>
+									</div>
+									<div class="clearfix"></div>								
+								</div>
+								<p>By logging in you agree to our <span>Terms and Conditions</span> and <span>Privacy Policy</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //signup -->
+	 <!-- /w3layouts-agile -->
+		<!-- main content start-->
+		<div class="main-content">
+			<!-- header-starts -->
+			<div class="header-section">
+			<!--toggle button start-->
+			<a class="toggle-btn  menu-collapsed"><i class="fa fa-bars"></i></a>
+			<!--toggle button end-->
+			<!--notification menu start -->
+				<div class="menu-right">
+					<div class="profile_details">		
+						  <div class="col-md-4 serch-part">
+								<div id="sb-search" class="sb-search">
+									<form action="#" method="post">
+										<input class="sb-search-input" placeholder="Search" type="search" name="search" id="search">
+										<input class="sb-search-submit" type="submit" value="">
+										<span class="sb-icon-search"> </span>
+									</form>
+								</div>
+							</div>
+							  <!-- search-scripts -->
+									<script src="js/classie.js"></script>
+									<script src="js/uisearch.js"></script>
+										<script>
+											new UISearch( document.getElementById( 'sb-search' ) );
+										</script>
+									<!-- //search-scripts -->
+											 <!---->
+											  <div class="col-md-4 player">
+													<div class="audio-player">
+														<audio id="audio-player"  controls="controls">
+														<source src="media/Blue Browne.ogg" type="audio/ogg"></source>
+																<source src="media/Blue Browne.mp3" type="audio/mpeg"></source>
+																<source src="media/Georgia.ogg" type="audio/ogg"></source>
+																<source src="media/Georgia.mp3" type="audio/mpeg"></source></audio>
+														</div>
+												<!---->
+												<script type="text/javascript">
+													$(function(){
+													  $('#audio-player').mediaelementplayer({
+														alwaysShowControls: true,
+														features: ['playpause','progress','volume'],
+														audioVolume: 'horizontal',
+														iPadUseNativeControls: true,
+														iPhoneUseNativeControls: true,
+														AndroidUseNativeControls: true
+													});
+												 });
+												</script>
+												<!--audio-->
+													<link rel="stylesheet" type="text/css" media="all" href="css/audio.css">
+													<script type="text/javascript" src="js/mediaelement-and-player.min.js"></script>
+													<!---->
+
+
+												<!--//-->
+												<ul class="next-top">
+													<li><a class="ar" href="#"> <img src="images/arrow.png" alt=""/></a></li>
+													<li><a class="ar2" href="#"><img src="images/arrow2.png" alt=""/></i></a></li>
+														
+											 </ul>	
+											</div>
+											<div class="col-md-4 login-pop">
+												<div id="loginpop"><!--<a href="index.php" id="loginButton"><span>Upload <i class="arrow glyphicon glyphicon-chevron-right"></i></span></a>--><a class="top-sign" href="index.php">Upload</a>
+														
+												</div>
+
+											</div>
+										<div class="clearfix"> </div>
+								</div>
+							<!-------->
+						</div>
+					<div class="clearfix"></div>
+				</div>
+					 <!-- /w3l-agile -->
+				<!--notification menu end -->
+				<!-- //header-ends -->
+<<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-3 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="http://lorempixel.com/100/100/people/9/">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        User name
+                    </div>
+                    <div class="desc">Artist desc1</div>
+                    <div class="desc">Artist desc1</div>
+                    <div class="desc">Artist desc1</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" rel="publisher"
+                       href="https://plus.google.com/shahnuralam">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a class="btn btn-primary btn-instagram btn-sm" href="https://twitter.com/webmaniac">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+	</div>
+</div>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+	<div class="upload">
+		<label for="exampleInputFile">File input</label>
+            <input type="file" id="exampleInputFile">
+        <p class="help-block">...</p>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </div>
+</form>
+
+<div class="browse">
+<div class="container">
+<script type="text/javascript">
+	(function ($) {
+    $.fn.extend({
+        tableAddCounter: function (options) {
+
+            // set up default options 
+            var defaults = {
+                title: '#',
+                start: 1,
+                id: false,
+                cssClass: false
+            };
+
+            // Overwrite default options with user provided
+            var options = $.extend({}, defaults, options);
+
+            return $(this).each(function () {
+                // Make sure this is a table tag
+                if ($(this).is('table')) {
+
+                    // Add column title unless set to 'false'
+                    if (!options.title) options.title = '';
+                    $('th:first-child, thead td:first-child', this).each(function () {
+                        var tagName = $(this).prop('tagName');
+                        $(this).before('<' + tagName + ' rowspan="' + $('thead tr').length + '" class="' + options.cssClass + '" id="' + options.id + '">' + options.title + '</' + tagName + '>');
+                    });
+
+                    // Add counter starting counter from 'start'
+                    $('tbody td:first-child', this).each(function (i) {
+                        $(this).before('<td>' + (options.start + i) + '</td>');
+                    });
+
+                }
+            });
+        }
+    });
+})(jQuery);
+
+$(document).ready(function () {
+    $('.table').tableAddCounter();
+    $.getScript("http://code.jquery.com/ui/1.9.2/jquery-ui.js").done(function (script, textStatus) { $('tbody').sortable();$(".alert-info").alert('close');$(".alert-success").show(); });
+});
+
+</script>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-info">
+                Please Wait...</div>
+            <div class="alert alert-success" style="display:none;">
+                <span class="glyphicon glyphicon-ok"></span> Arrange Your Songs</div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>
+                            User Songs
+                        </th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="active">
+                        <td>
+                            Column content
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>
+                            Column content
+                        </td>
+                        
+                    </tr>
+                    <tr class="">
+                        <td>
+                            Column content
+                        </td>
+                       
+                    </tr>
+                    <tr>
+                        <td>
+                            Column content
+                        </td>
+                        
+                    </tr>
+                    <tr class="">
+                        <td>
+                            Column content
+                        </td>
+                       
+                    </tr>
+                    <tr>
+                        <td>
+                            Column content
+                        </td>
+                        
+                    </tr>
+                    <tr class="">
+                        <td>
+                            Column content
+                        </td>
+                      
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+
+<form>
+             
+					<!--//End-albums-->
+
+
+			  <!--body wrapper end-->
+			       <div class="footer two">
+				<div class="footer-grid">
+					<h3>Navigation</h3>
+					<ul class="list1">
+					  <li><a href="index.html">Home</a></li>
+					  <li><a href="radio.html">All Songs</a></li>
+					  <li><a href="browse.html">Albums</a></li>
+					  <li><a href="radio.html">New Collections</a></li>
+					  <li><a href="blog.html">Blog</a></li>
+					  <li><a href="contact.html">Contact</a></li>
+				    </ul>
+				</div>
+				
+					 <div class="footer-grid footer-grid_last">
+						<h3>About Us</h3>
+						<p class="footer_desc">Diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat enim ad minim veniam,.</p>
+						<p class="f_text">Phone:  &nbsp;&nbsp;&nbsp;00-250-2131</p>
+						<p class="email">Email : &nbsp;<span><a href="mailto:mail@example.com">info(at)mailing.com</a></span></p>	
+					 </div>
+					 <div class="clearfix"> </div>
+				</div>
+			</div>
+		<!-- /wthree-agile -->
+        <!--footer section start-->
+			
+        <!--footer section end-->
+
+      <!-- main content end-->
+   </section>
+  
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
